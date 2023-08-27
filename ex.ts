45cd77aa3c044dd95e250f3091b2a0027e636d3b
zevik <zevik2001@gmail.com>
2023-08-27 10:29:49 +0300

    q 3

--- ex.ts
+++ ex.ts
@@ -1,23 +1,26 @@
 class Rectangle {
-hight:number;
-width:number;
+  hight: number;
+  width: number;
+  constructor(hight: number, width: number) {
+    this.hight = hight;
+    this.width = width;
+  }
+  arae() {
+    return this.hight * this.width;
+  }
+  info() {
+    return "this is a Rectangle";
+  }
+}
+class Square extends Rectangle {
+  rib: number;
 
-constructor(hight:number,width:number) {
-  this.hight=hight
-  this.width=width
+  constructor(hight: number, width: number, rib: number) {
+    super(hight, width);
+    this.rib = rib;
+  }
+  arae() {
+    return this.rib * 4;
+  }
 }
- arae(){
-  return this.hight*this.width;
- }
-}
-class Square extends Rectangle{
-rib:number;
 
-constructor(hight:number,width:number,rib:number) {
-  super(hight,width);
-  this.rib=rib
-}
-arae(){
-  return this.rib*4;
- }
-}
