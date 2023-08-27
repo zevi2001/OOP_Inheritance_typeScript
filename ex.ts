601a4513346cb4e5f0aa8a4241943594306333ec
zevik <zevik2001@gmail.com>
2023-08-27 11:38:16 +0300

    q 5

--- ex.ts
+++ ex.ts
@@ -1,56 +1,61 @@
-class Rectangle {
-  hight: number;
-  width: number;
-  constructor(hight: number, width: number) {
-    this.hight = hight;
-    this.width = width;
-  }
-  arae() {
-    return this.hight * this.width;
-  }
-  info() {
-    return "this is a Rectangle";
-  }
-  scale(num: number, str: string) {
-    if (str === "hight") {
-      this.hight = num;
-    } else if (str === "width") {
-      this.width = num;
-    }
-  }
-  
-}
-class Square extends Rectangle {
-  rib: number;
+// class Rectangle {
+//   hight: number;
+//   width: number;
+//   constructor(hight: number, width: number) {
+//     this.hight = hight;
+//     this.width = width;
+//   }
+//   arae() :number{
+//     return this.hight * this.width;
+//   }
+//   info():string {
+//     return "this is a Rectangle";
+//   }
+//   scale(num: number, str: string) {
+//     if (str === "hight") {
+//       this.hight = num;
+//     } else if (str === "width") {
+//       this.width = num;
+//     }
+//   }
 
-  constructor(hight: number, width: number, rib: number) {
-    super(hight, width);
-    this.rib = rib;
-  }
-  arae() {
-    return this.rib * 4;
-  }
-  info() {
-    return "this is a Rectangle";
-  }
-}
-class Shape extends Rectangle {
-  info() {
+// }
+
+class Shape {
+  draw(): string {
     return "this is a Shape";
   }
 }
-class ColoredRectangle extends Rectangle {
-  color: string;
-  constructor(color: string, hight: number, width: number) {
-    super(hight, width);
-    this.color = color;
-  }
-  info() {
-    return "this is a Rectangle with " + this.color + " color";
+class Square extends Shape {
+  // rib: number;
+  // constructor(hight: number, width: number, rib: number) {
+  //   super(hight, width);
+  //   this.rib = rib;
+  // }
+  // arae() :number{
+  //   return Math.pow(this.rib,this.rib);
+  // }
+
+  draw() {
+    return "this is a Rectangle";
   }
 }
-class Triangle extends Shape  {
-  info() {
+class Triangle extends Shape {
+  draw() {
     return "this is a Triangle";
   }
 }
+class Circle extends Shape {
+  draw() {
+    return "this is a Circle";
+  }
+}
+function renderShapes(array: Shape[]): void {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i].draw());
+  }
+}
+const S = new Square();
+const T = new Triangle();
+const SI = new Circle();
+renderShapes([S, T, SI]);
