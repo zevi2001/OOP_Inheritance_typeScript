a7d1f68a1a16294c5e3b2a9d018ada5728dfa3a6
zevik <zevik2001@gmail.com>
2023-08-27 10:19:05 +0300

    q 2

--- ex.ts
+++ ex.ts
@@ -1,14 +1,23 @@
 class Rectangle {
 hight:number;
 width:number;
-/**
- *
- */
+
 constructor(hight:number,width:number) {
   this.hight=hight
   this.width=width
 }
  arae(){
   return this.hight*this.width;
+ }
+}
+class Square extends Rectangle{
+rib:number;
+
+constructor(hight:number,width:number,rib:number) {
+  super(hight,width);
+  this.rib=rib
+}
+arae(){
+  return this.rib*4;
  }
 }
